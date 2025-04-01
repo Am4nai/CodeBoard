40364e2013266963d1cd1e8ce28bd1d104dbd02c
Andrey Malyshev <andr3y044@gmail.com>
2025-04-01 16:18:09 +0300

    CodeBoard

---
+++ src/main/java/com/codeboard/codeboard_backend/CodeBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codeboard.codeboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBoardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBoardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeboard/codeboard_backend/CodeBoardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeboard.codeboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBoardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
