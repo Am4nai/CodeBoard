3ab0224559075a2d3bb5ff810360e054a2a6309d
Andrey <Andr3y044@gmail.com>
2025-05-13 11:44:14 +0300

    CodeBoard

--- src/test/java/com/codeboard/codeboard_backend/UserRepositoryTests.java
+++
@@ -1,9 +0,0 @@
-package com.codeboard.codeboard_backend;
-
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-
-@SpringBootTest(properties = "spring.profiles.active=test")
-class UserRepositoryTests {
-}
