d0cd97eef6802b594f0ae7ab4a3da03285da456a
Andrey <Andr3y044@gmail.com>
2025-05-13 11:43:49 +0300

    CodeBoard

--- src/test/java/com/codeboard/codeboard_backend/UserRepositoryTests.java
+++ src/test/java/com/codeboard/codeboard_backend/UserRepositoryTests.java
@@ -1,22 +1,8 @@
 package com.codeboard.codeboard_backend;
 
-import com.codeboard.codeboard_backend.dto.request.PostCreateDto;
-import com.codeboard.codeboard_backend.dto.request.UserCreateDto;
-import com.codeboard.codeboard_backend.dto.response.AuthResponseDto;
-import com.codeboard.codeboard_backend.model.Post;
-import com.codeboard.codeboard_backend.model.User;
-import com.codeboard.codeboard_backend.model.enums.UserRoleEnum;
-import com.codeboard.codeboard_backend.repository.PostRepository;
-import com.codeboard.codeboard_backend.repository.UserRepository;
-import com.codeboard.codeboard_backend.service.AuthService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
 class UserRepositoryTests {
