fbf1062f15790b0b19c8c606e0e3261a6853dda3
Andrey <Andr3y044@gmail.com>
2025-05-13 18:49:00 +0300

    CodeBoard

--- src/main/java/com/codeboard/codeboard_backend/config/SecurityConfig.java
+++ src/main/java/com/codeboard/codeboard_backend/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://code-board-frontend.vercel.app/")); // Разрешённый домен клиента
+        configuration.setAllowedOrigins(List.of("https://code-board-frontend.vercel.app")); // Разрешённый домен клиента
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Разрешённые методы
         configuration.setAllowedHeaders(List.of("*")); // Разрешённые заголовки
         configuration.setAllowCredentials(true); // Если требуется отправлять куки
